4978d0e9b16a35ec85a0a03f8d4122b838ae2c9f
Marderon123 <alexmonu2000@gmail.com>
2024-02-12 10:41:34 +0100

    GoogleQuestion - Done

--- GoogleQuesions.js
+++ GoogleQuesions.js
@@ -8,17 +8,22 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
 
 function firstRecurringCharacter(input) {
+    let charSet = new Set();
     for (let i = 0; i < input.length; i++) {
-        for (let j = i + 1; j < input.length; j++) {
-            if (input[i] === input[j]) {
-                return input[i];
-            }
+        if (charSet.has(input[i])) {
+            return input[i]
+        } else {
+            charSet.add(input[i])
         }
     }
     return undefined;
-}//0(n^2)
+}//0(n)
+console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]))
 
 function firstRecurringCharacter2(input) {
     let map = {};
@@ -34,9 +39,4 @@ function firstRecurringCharacter2(input) {
     return undefined
 }//0(n)
 
-//console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]))
 console.log(firstRecurringCharacter2([2, 5, 1, 2, 3, 5, 1, 2, 4]))
-
-//Bonus... What if we had this:
-// [2,5,5,2,3,5,1,2,4]
-// return 5 because the pairs are before 2,2
